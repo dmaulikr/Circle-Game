cc5b788ea099a2ee977d87deca32d4a53566925e
Jordan Atkins <oxafemble@gmail.com>
2014-02-24 11:26:14 +1100

    Added game source URL

--- qViewController.m
+++ qViewController.m
@@ -9,21 +9,27 @@
 #import "qViewController.h"
 
 @interface qViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *viewWeb;
 
 @end
 
 @implementation qViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    NSString *fullURL = @"http://127.0.0.1/Circle%20Game/"; // I really should upload the files soon.
+    NSURL *url = [NSURL URLWithString:fullURL];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    [_viewWeb loadRequest:requestObj];
 }
+
+- (BOOL)prefersStatusBarHidden {return YES;}
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
+
+
 
 @end
